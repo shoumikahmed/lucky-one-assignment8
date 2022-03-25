0a99f00d6b632638d582593f5e56fb60ece64a0d
shoumikahmed <shoumikahmed103@gmail.com>
2022-03-26 01:37:03 +0600

    qoutetion

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Cart = (props) => {
     const { cart } = props
 
-    let name = []
+    let name = ''
     for (const product of cart) {
         name = name + product.name
     }
