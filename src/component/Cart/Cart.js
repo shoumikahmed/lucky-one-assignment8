dde733b280dffbe0b153e2cb058d69e7cf77db5e
shoumikahmed <shoumikahmed103@gmail.com>
2022-03-25 23:49:51 +0600

    name added on cart

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Cart = (props) => {
     const { cart } = props
+
+    let name = ''
+    for (const product of cart) {
+        name = name + product.name
+    }
+
     return (
         <div className='cart'>
             <h4 className='my-4'>Selected Watches</h4>
+            <h5>{name}</h5>
         </div>
     );
 };
